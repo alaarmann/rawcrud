0a3b2ca5f7347bb044e2713c7737dd005806c214
Alexander Laarmann <alaarmann@gmx.net>
2016-01-10 06:45:08 +0100

    Remove unnecessary function levels part 1

--- src/js/component/BaseComponent.js
+++ src/js/component/BaseComponent.js
@@ -10,163 +10,158 @@ var $ = require('jquery');
   getContainerElement()
 */
 
-module.exports = (function () {
+module.exports =  function (){
   'use strict';
-
-  var create = function (){
-    var internalRender;
-    var createUserTriggeredEventHandler;
-    var createUserTriggeredActionEventHandler;
-    var that = this;
+  var internalRender;
+  var createUserTriggeredEventHandler;
+  var createUserTriggeredActionEventHandler;
+  var that = this;
 
     
-    createUserTriggeredEventHandler = function(aHandler){
-      var activatedId;
-      return function(){
-          activatedId = $(this).find('.propId').text();
-          aHandler(activatedId);
-        };
+  createUserTriggeredEventHandler = function(aHandler){
+    var activatedId;
+    return function(){
+      activatedId = $(this).find('.propId').text();
+      aHandler(activatedId);
     };
-
-    /* Attach handler for user triggered event */
-    that.bindActivate = function (){
-      var each;
-      var propertyName;
-      var className;
-      var containerElement = that.getContainerElement();
-
-      for (each in that) {
-        if (each.indexOf('activate') !== 0) {
-          continue;
-        }
-        if (!that.hasOwnProperty(each)) {
-          continue;
-        }
-        if (typeof(that[each]) !== 'function') {
-          continue;
-        }
-        propertyName = each.slice('activate'.length);
-
-        className = 'prop' + propertyName;
-
-        containerElement.on('dblclick', '.' + className, createUserTriggeredEventHandler(that[each]));
-      }
-
-    };
-
-    createUserTriggeredActionEventHandler = function(aHandler){
-      return function(){
-          aHandler();
-          return false;
-        };
-    };
-
-    that.bindAction = function (){
-      var each;
-      var className;
-      var containerElement = that.getContainerElement();
-
-      for (each in that) {
-        if (each.indexOf('action') !== 0) {
-          continue;
-        }
-        if (!that.hasOwnProperty(each)) {
-          continue;
-        }
-        if (typeof(that[each]) !== 'function') {
-          continue;
-        }
-
-        if (each === 'actionDefault') {
-          containerElement.on( 'submit', 'form', createUserTriggeredActionEventHandler(that[each]));
-        } else {
-          className = each;
-
-          containerElement.on('click', '.' + className, createUserTriggeredActionEventHandler(that[each]));
-          containerElement.on(each, createUserTriggeredActionEventHandler(that[each]));
-        }
-
-      }
-
-    };
-
-
-    /* Wire model's properties to output fields in view */ 
-    internalRender = function(aInternalContainerElement, aModel){
-      var each;
-      var propertyName;
-      var className;
-      var value;
-      var prototypeItem;
-      var newItem;
-      var i;
-
-
-      for (each in aModel) {
-        if (each.indexOf('get') !== 0) {
-          continue;
-        }
-        if (!aModel.hasOwnProperty(each)) {
-          continue;
-        }
-        if (typeof(aModel[each]) !== 'function') {
-          continue;
-        }
-
-        propertyName = each.slice('get'.length);
-        //console.log('propertyName: ' + propertyName);
-        className = 'prop' + propertyName;
-        value = aModel[each]();
-        // Treat list value differently than single value
-        if (value && value.constructor === Array){
-          prototypeItem = aInternalContainerElement.find('.rawcrud-list.' + className);
-          prototypeItem.nextAll().remove();
-          // FIXME do not clone id attributes
-          for(i=0;i<value.length;i+=1){
-            newItem = prototypeItem.clone().removeClass('rawcrud-list').appendTo(prototypeItem.parent());
-            internalRender(newItem, value[i]);
-          }
-        } else {
-          aInternalContainerElement.find('input.' + className).val(value);
-          aInternalContainerElement.find('td.' + className).text(value);
-        }
-        
-      }
-    };
-
-    that.render = function() {
-      var containerElement = that.getContainerElement();
-      return internalRender(containerElement, that.model);
-    };
-
-    /* Wire model's properties to input fields in view */ 
-    that.evaluate = function(){
-      var each;
-      var propertyName;
-      var className;
-      var containerElement = that.getContainerElement();
-
-      for (each in that.model) {
-        if (each.indexOf('set') !== 0) {
-          continue;
-        }
-        if (!that.model.hasOwnProperty(each)) {
-          continue;
-        }
-        if (typeof(that.model[each]) !== 'function') {
-          continue;
-        }
-        propertyName = each.slice('set'.length);
-        className = 'prop' + propertyName;
-
-        that.model[each](containerElement.find('input.' + className).val());
-      }
-
-    };
-
-    return that;
   };
- 
-  return create;
-}());
+
+  /* Attach handler for user triggered event */
+  that.bindActivate = function (){
+    var each;
+    var propertyName;
+    var className;
+    var containerElement = that.getContainerElement();
+
+    for (each in that) {
+      if (each.indexOf('activate') !== 0) {
+        continue;
+      }
+      if (!that.hasOwnProperty(each)) {
+        continue;
+      }
+      if (typeof(that[each]) !== 'function') {
+        continue;
+      }
+      propertyName = each.slice('activate'.length);
+
+      className = 'prop' + propertyName;
+
+      containerElement.on('dblclick', '.' + className, createUserTriggeredEventHandler(that[each]));
+    }
+
+  };
+
+  createUserTriggeredActionEventHandler = function(aHandler){
+    return function(){
+      aHandler();
+      return false;
+    };
+  };
+
+  that.bindAction = function (){
+    var each;
+    var className;
+    var containerElement = that.getContainerElement();
+
+    for (each in that) {
+      if (each.indexOf('action') !== 0) {
+        continue;
+      }
+      if (!that.hasOwnProperty(each)) {
+        continue;
+      }
+      if (typeof(that[each]) !== 'function') {
+        continue;
+      }
+
+      if (each === 'actionDefault') {
+        containerElement.on( 'submit', 'form', createUserTriggeredActionEventHandler(that[each]));
+      } else {
+        className = each;
+
+        containerElement.on('click', '.' + className, createUserTriggeredActionEventHandler(that[each]));
+        containerElement.on(each, createUserTriggeredActionEventHandler(that[each]));
+      }
+
+    }
+
+  };
+
+
+  /* Wire model's properties to output fields in view */ 
+  internalRender = function(aInternalContainerElement, aModel){
+    var each;
+    var propertyName;
+    var className;
+    var value;
+    var prototypeItem;
+    var newItem;
+    var i;
+
+
+    for (each in aModel) {
+      if (each.indexOf('get') !== 0) {
+        continue;
+      }
+      if (!aModel.hasOwnProperty(each)) {
+        continue;
+      }
+      if (typeof(aModel[each]) !== 'function') {
+        continue;
+      }
+
+      propertyName = each.slice('get'.length);
+      //console.log('propertyName: ' + propertyName);
+      className = 'prop' + propertyName;
+      value = aModel[each]();
+      // Treat list value differently than single value
+      if (value && value.constructor === Array){
+        prototypeItem = aInternalContainerElement.find('.rawcrud-list.' + className);
+        prototypeItem.nextAll().remove();
+        // FIXME do not clone id attributes
+        for(i=0;i<value.length;i+=1){
+          newItem = prototypeItem.clone().removeClass('rawcrud-list').appendTo(prototypeItem.parent());
+          internalRender(newItem, value[i]);
+        }
+      } else {
+        aInternalContainerElement.find('input.' + className).val(value);
+        aInternalContainerElement.find('td.' + className).text(value);
+      }
+        
+    }
+  };
+
+  that.render = function() {
+    var containerElement = that.getContainerElement();
+    return internalRender(containerElement, that.model);
+  };
+
+  /* Wire model's properties to input fields in view */ 
+  that.evaluate = function(){
+    var each;
+    var propertyName;
+    var className;
+    var containerElement = that.getContainerElement();
+
+    for (each in that.model) {
+      if (each.indexOf('set') !== 0) {
+        continue;
+      }
+      if (!that.model.hasOwnProperty(each)) {
+        continue;
+      }
+      if (typeof(that.model[each]) !== 'function') {
+        continue;
+      }
+      propertyName = each.slice('set'.length);
+      className = 'prop' + propertyName;
+
+      that.model[each](containerElement.find('input.' + className).val());
+    }
+
+  };
+
+  return that;
+};
 
--- src/js/component/CrudPresenter.js
+++ src/js/component/CrudPresenter.js
@@ -9,28 +9,20 @@ var createRetriever = require('./RetrieverPresenter.js');
 var RegistrationCapable = require('./RegistrationCapable.js');
 var makeNavigationCapable = require('./NavigationCapable.js');
 
-
-
-module.exports = (function () {
+module.exports = function (aContainerElement){
   'use strict';
+  var component;
 
-  var create = function (aContainerElement){
-    var component;
+  component = {getContainerElement : function(){return aContainerElement;}};
+  RegistrationCapable.apply(component);
+  makeNavigationCapable.apply(component);
 
-    component = {getContainerElement : function(){return aContainerElement;}};
-    RegistrationCapable.apply(component);
-    makeNavigationCapable.apply(component);
+  component.register('editor', createEditor);
+  component.register('result', createRetriever);
 
+  // Open Retriever
+  component.openNewScreen('result');
 
-    component.register('editor', createEditor);
-    component.register('result', createRetriever);
-
-    // Open Retriever
-    component.openNewScreen('result');
-
-    return;
-  };
- 
-  return create;
-}());
+  return;
+};
 
