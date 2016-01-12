0aaf7d07f915f179f9767d8dc7930393c462b9bb
Alexander Laarmann <alaarmann@gmx.net>
2016-01-12 04:16:39 +0100

    Remove unnecessary function levels part 2

--- src/js/component/EditorPresenter.js
+++ src/js/component/EditorPresenter.js
@@ -10,64 +10,59 @@ var makeNavigationCapable = require('./NavigationCapable.js');
 var repository = require('../model/repository.js');
 
 
-module.exports = (function () {
+module.exports =  function (aContainerElement){
   'use strict';
+  var save;
+  var processForm;
+  var view;
+  var component = {getContainerElement : function(){return aContainerElement;}};
 
-  var create = function (aContainerElement){
-    var save;
-    var processForm;
-    var view;
-    var component = {getContainerElement : function(){return aContainerElement;}};
+  view = createView(aContainerElement);
+  component.getView = function(){return view;};
 
-    view = createView(aContainerElement);
-    component.getView = function(){return view;};
+  createBaseComponent.apply(component);
+  makeNavigationCapable.apply(component);
 
-    createBaseComponent.apply(component);
-    makeNavigationCapable.apply(component);
-
-    // User triggered action, will be bound to .actionProcess
-    component.actionProcess = function(){
-      processForm();
-    };
-    component.actionDefault = function(){
-      processForm();
-    };
-    component.actionCancel = function(){
-      // Screen flow
-      component.closeScreen();
-    };
-    // Callback for screen flow (Navigation triggered action)
-    component.openScreen = function(aModelToWorkOn){
-      var view; 
-      if (typeof component.getView === 'function'){
-        view = component.getView();
-      }
-      // model is set HERE!
-      component.model = aModelToWorkOn;
-      component.render();
-      if (view && typeof view.open === 'function'){
-        view.open();
-      }
-    };
-
-
-    save = repository.save;
-
-    processForm = function(){
-      component.evaluate();
-      save(component.model);
-
-      // Screen flow
-      component.closeScreen();
-    };
-
-    // bind actions once at construction time
-    component.bindAction();
-    component.bindActivate();
-
-    return component;
+  // User triggered action, will be bound to .actionProcess
+  component.actionProcess = function(){
+    processForm();
   };
- 
-  return create;
-}());
+  component.actionDefault = function(){
+    processForm();
+  };
+  component.actionCancel = function(){
+    // Screen flow
+    component.closeScreen();
+  };
+  // Callback for screen flow (Navigation triggered action)
+  component.openScreen = function(aModelToWorkOn){
+    var view; 
+    if (typeof component.getView === 'function'){
+      view = component.getView();
+    }
+    // model is set HERE!
+    component.model = aModelToWorkOn;
+    component.render();
+    if (view && typeof view.open === 'function'){
+      view.open();
+    }
+  };
+
+
+  save = repository.save;
+
+  processForm = function(){
+    component.evaluate();
+    save(component.model);
+
+    // Screen flow
+    component.closeScreen();
+  };
+
+  // bind actions once at construction time
+  component.bindAction();
+  component.bindActivate();
+
+  return component;
+};
 
--- src/js/component/RetrieverPresenter.js
+++ src/js/component/RetrieverPresenter.js
@@ -10,57 +10,52 @@ var makeNavigationCapable = require('./NavigationCapable.js');
 var repository = require('../model/repository.js');
 var createHeadItemFilter = require('../model/headItemFilter.js');
 
-module.exports = (function () {
+module.exports = function (aContainerElement){
   'use strict';
+  var component = {getContainerElement : function(){return aContainerElement;}};
+  var processForm;
+  var view;
+  view = createView(aContainerElement);
+  component.getView = function(){return view;};
 
-  var create = function (aContainerElement){
-    var component = {getContainerElement : function(){return aContainerElement;}};
-    var processForm;
-    var view;
-    view = createView(aContainerElement);
-    component.getView = function(){return view;};
+  createBaseComponent.apply(component);
+  makeNavigationCapable.apply(component);
 
-    createBaseComponent.apply(component);
-    makeNavigationCapable.apply(component);
-
-    // User triggered event
-    component.activateHeadItems = function(aActivatedId){
-      editRecordAt(aActivatedId);
-    };
-    component.actionCreate = function(){
-      var newModelToWorkOn = repository.createHeadItem();
-      component.openNewScreen('editor', [ newModelToWorkOn ]);
-    };
-    component.actionFind = function(){
-      processForm();
-    };
-    component.actionDefault = function(){
-      processForm();
-    };
-
-    processForm = function(){
-      var headItemFilter = createHeadItemFilter();
-      component.model = headItemFilter;
-      component.evaluate();
-
-      console.log('retrieve with filter owner=' + headItemFilter.getOwner() + ' and reference=' + headItemFilter.getReference());
-      component.model = {getHeadItems : function(){ return repository.retrieve(headItemFilter); }};
-      component.render();
-    };
-    component.openScreen = processForm;
-    component.showScreen = processForm;
-
-    var editRecordAt = function (aId){
-      var modelToWorkOn = repository.startWorkOn(aId);
-      component.openNewScreen('editor', [ modelToWorkOn ]);
-    };
-
-    // bind actions once at construction time
-    component.bindAction();
-    component.bindActivate();
-    return component;
+  // User triggered event
+  component.activateHeadItems = function(aActivatedId){
+    editRecordAt(aActivatedId);
   };
- 
-  return create;
-}());
+  component.actionCreate = function(){
+    var newModelToWorkOn = repository.createHeadItem();
+    component.openNewScreen('editor', [ newModelToWorkOn ]);
+  };
+  component.actionFind = function(){
+    processForm();
+  };
+  component.actionDefault = function(){
+    processForm();
+  };
+
+  processForm = function(){
+    var headItemFilter = createHeadItemFilter();
+    component.model = headItemFilter;
+    component.evaluate();
+
+    console.log('retrieve with filter owner=' + headItemFilter.getOwner() + ' and reference=' + headItemFilter.getReference());
+    component.model = {getHeadItems : function(){ return repository.retrieve(headItemFilter); }};
+    component.render();
+  };
+  component.openScreen = processForm;
+  component.showScreen = processForm;
+
+  var editRecordAt = function (aId){
+    var modelToWorkOn = repository.startWorkOn(aId);
+    component.openNewScreen('editor', [ modelToWorkOn ]);
+  };
+
+  // bind actions once at construction time
+  component.bindAction();
+  component.bindActivate();
+  return component;
+};
 
